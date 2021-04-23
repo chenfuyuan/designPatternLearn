46e17fe310c78f6a9d63b1d685821a38c07f6b19
chenfuyuan <chenfuyuan0713@163.com>
2021-04-23 23:34:10 +0800

    feat(单例模式): 静态内部类-懒汉式单例

---
+++ src/main/java/com/learn/designpattern/singleton/LazyStaticInnerClassSingleton.java
@@ -0,0 +1,26 @@
+package com.learn.designpattern.singleton;
+
+/**
+ * @Description: 静态内部类
+ * @Author: chenfuyuan
+ * @Date: 2021/4/23 23:28
+ */
+public class LazyStaticInnerClassSingleton {
+    //使用LazyStaticInnerClass类时，默认会先初始化内部类
+    //如果没使用，则内部类是不加载的
+    private LazyStaticInnerClassSingleton() {
+
+    }
+
+    private static LazyStaticInnerClassSingleton getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+
+
+
+    private static class LazyHolder{
+        private static final LazyStaticInnerClassSingleton INSTANCE = new LazyStaticInnerClassSingleton();
+    }
+
+}
+
